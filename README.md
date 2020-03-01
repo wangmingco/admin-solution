b40003eff2459320a7f6ae8d2f11c5db0d8976b8
wangming01 <wangming01@finupgroup.com>
2020-03-01 14:16:08 +0800

    提交第一个版本

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # admin-solution
+
+前端采用vue-element-admin框架实现
+
+后端采用SpringBoot, Mybatis, Shiro, Druid框架实现
---
+++ admin-server/src/main/java/co/wangming/adminserver/AdminServerApplication.java
@@ -0,0 +1,13 @@
+package co.wangming.adminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
