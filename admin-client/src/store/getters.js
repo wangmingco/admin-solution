5622c94b3772c3cf11645b76e651ead8ad90bccc
wangming01 <wangming01@finupgroup.com>
2020-03-12 18:41:54 +0800

    完成前端动态路由功能, 包含侧边栏. 目前是前端通过不同角色筛选出不同权限路径构建路由, 下一步将整个路由的构建都放到后端来

--- admin-client/src/store/getters.js
+++ admin-client/src/store/getters.js
@@ -3,6 +3,7 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   avatar: state => state.user.avatar,
+  permission_routes: state => state.permission.routes,
   name: state => state.user.name
 }
 export default getters
