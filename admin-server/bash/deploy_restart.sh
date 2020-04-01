ddda89d663d9d1d0a234e7c9cadee0a16f04d9c0
wangming01 <wangming01@finupgroup.com>
2020-04-01 10:20:38 +0800

    修改打包脚本

--- admin-server/bash/deploy_restart.sh
+++ admin-server/bash/deploy_restart.sh
@@ -2,22 +2,27 @@
 
 #!/usr/bin/env bash
 
-GIT_URL=git@github.com:wangmingco/admin-solution.git
 WORKSPACE_NAME=admin-solution
 SERVER_NAME=admin-server
-SERVER_PATH=${WORKSPACE_NAME}/${SERVER_NAME}
-JAR_PATH=admin-server-0.1-exec.jar
+JAR_NAME=admin-server-0.1-exec.jar
 
-WORK_HOME=`pwd`/${SERVER_PATH}
+WORK_HOME='/data/'${WORKSPACE_NAME}
+SERVER_HOME=${WORK_HOME}/${SERVER_NAME}
 
 echo "WORK_HOME --> " ${WORK_HOME}
-echo "GIT_URL --> " ${GIT_URL}
-echo "WORKSPACE_NAME --> " ${WORKSPACE_NAME}
-echo "SERVER_NAME --> " ${SERVER_NAME}
-echo "SERVER_PATH --> " ${SERVER_PATH}
-echo "JAR_PATH --> " ${JAR_PATH}
+echo "SERVER_HOME --> " ${SERVER_HOME}
+echo "JAR_NAME --> " ${JAR_NAME}
+
+function goWorkHome() {
+     cd ${WORK_HOME}
+}
+
+function goServerHome() {
+     cd ${SERVER_HOME}
+}
 
 function stop() {
+    goServerHome
     echo "运行中的 ${SERVER_NAME} 进程:"
     jps -l | grep "${SERVER_NAME}"
     pid=`cat server.pid`
@@ -28,27 +33,21 @@ function stop() {
 }
 
 function download() {
-    if [ -e "${WORKSPACE_NAME}" ]; then
-        echo "开始更新代码"
-        cd ${WORKSPACE_NAME}
-        git pull
-        cd ${WORK_HOME}
-    else
-        echo "开始检出代码"
-        git clone ${GIT_URL}
-    fi
+    echo "开始更新代码 -> " ${WORK_HOME}
+    goWorkHome
+    git pull
 }
 
 function build() {
-    echo "开始构建代码"
-    cd ${SERVER_PATH}
+    echo "开始构建代码 -> " ${SERVER_HOME}
+    goServerHome
     mvn clean package
     echo "构建代码完成"
 }
 
 function start() {
     echo "开始启动服务"
-    nohup java -jar ./target/${JAR_PATH} >server.log 2>&1 &
+    nohup java -jar ${SERVER_HOME}/target/${JAR_NAME} >server.log 2>&1 &
     jps -l | grep "${SERVER_NAME}" | awk '{print $1}' > server.pid
     jps -l | grep "${SERVER_NAME}"
     echo "启动服务完成"
